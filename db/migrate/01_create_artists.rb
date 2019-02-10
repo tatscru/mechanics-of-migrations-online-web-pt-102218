9c40fdae7196772b3100eb1c6b0e6248257b135b
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-10 04:00:46 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,4 +1,4 @@
-class CreateArtists < ActiveRecord::Migration[5.1]
+class CreateArtists < ActiveRecord::Migration[5.0]
   def change
     create_table :artists do |t|
     #table name is inputted as a symbol 
